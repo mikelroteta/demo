06c74665ed119e75e03f385cae3e31f5721eaac7
mikelroteta <mikelroteta@hotmail.com>
2019-06-19 12:36:30 +0200

    assert null

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,7 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Assert.assertNotNull(null);
 	}
 
 }
